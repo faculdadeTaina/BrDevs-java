7295136a38f531445e1ad50027473de93a39b470
Tainá Miranda <tainamirandaferreira@gmail.com>
2022-05-08 18:37:07 -0300

    teste

--- Main.java
+++ Main.java
@@ -3,20 +3,6 @@ class Main {
   public static void main(String[] args) {
 //crirando objeto da calls
 
-//instanciando 
-Cliente pessoa1=new Cliente('ola','ola2');
 
-pessoa1.setDescN("Teste");
-pessoa1.setDescP("teste 2");
-//preenchendo
-System.out.println(pessoa1.getDes());
-System.out.println(pessoa1.getNome());
-//pessoa1.descProjeto="Projeto sobre escola";
-//pessoa1.nomeProjeto="Escola sol nascente";
-
-    //System.out.println(pessoa1.descProjeto);
-    //System.out.println(pessoa1.nomeProjeto);
-
-  
   }
 }
