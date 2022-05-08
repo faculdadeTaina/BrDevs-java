14dfde1d09656f7ee34924fde89d574b4112df32
Tainá Miranda <tainamirandaferreira@gmail.com>
2022-05-08 19:26:14 -0300

    Inicio do menu

--- Main.java
+++ Main.java
@@ -1,7 +1,44 @@
 //class principal
+
 class Main {
   public static void main(String[] args) {
 //crirando objeto da calls
+//menu 
+
+String menu="Projeto";
+
+switch (menu){
+  case "Projeto":
+  Projeto projeto1=new Projeto("teste", "teste1", null, null);
+
+
+  projeto1.setNome("Projeto vida");
+  projeto1.setDescrição("História de vida");
+  projeto1.setTecnologia("Javascript");
+  
+  
+  System.out.println(projeto1.getNome());
+  System.out.println(projeto1.getDescrição());
+  System.out.println(projeto1.getTecnologia());
+  break;
+
+  case "Login":
+
+  break;
+  
+  case "Cadastro":
+
+  break;
+
+  case "Erro":
+
+  default:
+        System.out.println("Algo deu errado");
+
+
+}
+
+
 
 
   }
