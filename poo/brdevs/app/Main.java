d3b250b45643a2f5dc8a1e26a1c06a5e9dea6072
Tainá Miranda <tainamirandaferreira@gmail.com>
2022-06-18 11:02:08 -0300

    teste final

--- poo/brdevs/app/Main.java
+++ poo/brdevs/app/Main.java
@@ -40,33 +40,65 @@ System.out.printf("A sua escolha foi %s:",menu);
 
 switch (menu){
   case 1:
+  int pro1=0;
+  System.out.println("Chegou na parte de projetos:\n Digite 1 para fazer enviar: \n Digite para fazer excluir: \n Digite 3 para atualizar");
+pro1=leitor.nextInt();
+
 Projeto projeto1=new Projeto("teste", "teste1", null, null);
 System.out.println("Voce escolheu a opção Projeto no seu menu!.");
+if(pro1 ==1){
+  projeto1.enviarProjeto();
+}else if(pro1 ==2){
+  projeto1.excluirProjeto();
+}else if(pro1 == 3){
+projeto1.atualizarprojeto();
+}else{
+  System.out.println("Algo deu errado com o sistema");
 
-  projeto1.setNomeProjeto("Projeto vida");
-  projeto1.setDscProjeto("Quero um site para contar a história de vida das pessoas");
-  projeto1.setTecnologiaProjeto("Javascript");
-  
-  
-  System.out.println(projeto1.getNomeProjeto());
-  System.out.println(projeto1.getDscProjeto());
-  System.out.println(projeto1.getTecnologiaProjeto());
+}
+
   break;
 
   case 2:
-  System.out.println("Chegou na pate do programador");
-  Programador especialidade= new Programador("marcos", "marcos@gmail.com", "lo", "123", "");
-  
- 
-  break;
-  case 3:
-  System.out.println("Chegou na parte dos clientes");
+  int p1=0;
+  System.out.println("Chegou na parte do programador(a):\n Digite 1 para fazer cadastro: \n Digite para fazer login: \n Digite 3 para redefinir senha");
+p1=leitor.nextInt();
+System.out.printf("A sua escolha foi %s:",p1);
+
+  Programador programador1= new Programador("marcos", "marcos@gmail.com", "lo", "123", "");
+if(p1 == 1){
+  programador1.fazerCadastro();
+}else if(p1 == 2){
+  programador1.fazerLogin();
+}else if(p1 ==3){
+  programador1.redefinirSenha();
+}else{
+  System.out.println("Algo deu errado com o sistema");
+}
 
   break;
+  case 3:
+
+int cli=0;
+System.out.println("Chegou na parte dos clinetes:\n Digite 1 para fazer cadastro: \n Digite para fazer login: \n Digite 3 para redefinir senha");
+
+cli=leitor.nextInt();
+DevCliente cliente1= new DevCliente("Leo", "leo@gmail.com", "Leo", "123", "3");
+//System.out.printf("A sua escolha foi %s:",cli);
+if(cli ==1){
+  cliente1.fazerCadastro();
+}else if(cli ==2){
+  cliente1.fazerLogin();
+}else if(cli==3){
+  cliente1.redefinirSenha();
+}else{
+  System.out.println("Algo deu errado com o sistema");
+}
+break;
 
   case 0:
   default:
-        System.out.println("Algo deu errado");
+        System.out.println("Algo deu errado no sistema ou a sessão expirou!");
 
 }
 
